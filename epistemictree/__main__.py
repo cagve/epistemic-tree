81e2381d3f924c7e86f474dc07a06afb8c4b24de
cagve <carlos.aguilera13416@gmail.com>
2022-05-31 12:19:49 +0200

    [FIX] Print counter model

--- epistemictree/__main__.py
+++ epistemictree/__main__.py
@@ -1,9 +1,17 @@
 import plac
 from epistemictree import rules as rl
 from epistemictree import tui as t
+from epistemictree.parser import Formula
 
 def main():
     t.run()
+
+def test():
+    formula1 = Formula("Kap")
+    formula2 = Formula("-Kap")
+    formula3 = Formula("-KapKaq")
+    formula4 = Formula("--Kap-Kbq")
+    formula5 = Formula("Kap")
         
 
 if __name__ == '__main__':
