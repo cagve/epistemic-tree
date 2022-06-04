c2789124579ef9e98307dd4aff72e010954ca2df
cagve <carlos.aguilera13416@gmail.com>
2022-06-04 15:44:25 +0200

    V1

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+    name='Epistemic tableaux',
+    version='1.0.0',
+    description='A tool for find a model of the formulas',
+    author='Carlos',
+    author_email='carlos.aguilera@uma.es',
+    license='GNU GPLv3',
+    # classifiers=[
+    #     'Development Status :: 1 - Beta',
+    #     'Intended Audience :: Logicians',
+    #     'Topic :: Epistemic logic',
+    #     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    #     'Programming Language :: Python :: 3',
+    # ],
+    keywords='epistemic logic',
+    packages=['epistemictree'],
+)
